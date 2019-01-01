53a6222f70edc2e38b2fb911d462702782623af1
ashwek <ashwekswamy@protonmail.com>
2019-01-01 17:22:21 +0530

    Update infixToPostfix.py

--- STACKS/Python/infixToPostfix.py
+++ STACKS/Python/infixToPostfix.py
@@ -1,7 +1,7 @@
 """
 Output:
 
-Enter an Infix Equation = a + b ^c
+Enter an Infix Equation (space separated) = a + b ^ c
  Symbol  |  Stack  | Postfix
 ----------------------------
    a     |         | a
@@ -15,7 +15,7 @@ Enter an Infix Equation = a + b ^c
 	 a+b^c -> abc^+
 """
 
-def infix_2_postfix(Infix):
+def infix_2_postfix(infix):
     Stack = []
     Postfix = []
     priority = {'^':3, '*':2, '/':2, '%':2, '+':1, '-':1}       # Priority of each operator
@@ -25,28 +25,28 @@ def infix_2_postfix(Infix):
     print('Symbol'.center(8), 'Stack'.center(print_width), 'Postfix'.center(print_width), sep = " | ")
     print('-'*(print_width*3+7))
 
-    for x in Infix:
-        if(x.isalpha() or x.isdigit()): Postfix.append(x)       # if x is Alphabet / Digit, add it to Postfix
-        elif(x == '('): Stack.append(x)                         # if x is "(" push to Stack
-        elif(x == ')'):                                         # if x is ")" pop stack until "(" is encountered
-            while(Stack[-1] != '('):
+    for x in infix:
+        if x.isalpha() or x.isdigit() : Postfix.append(x)       # if x is Alphabet / Digit, add it to Postfix
+        elif x == '(' : Stack.append(x)                         # if x is "(" push to Stack
+        elif x == ')' :                                         # if x is ")" pop stack until "(" is encountered
+            while Stack[-1] != '(' :
                 Postfix.append( Stack.pop() )                   #Pop stack & add the content to Postfix
             Stack.pop()
         else:
-            if(len(Stack)==0): Stack.append(x)                  #If stack is empty, push x to stack
+            if len(Stack) == 0 : Stack.append(x)                  #If stack is empty, push x to stack
             else:
-                while( len(Stack) > 0 and priority[x] <= priority[Stack[-1]]):      # while priority of x is not greater than priority of element in the stack
+                while len(Stack) > 0 and priority[x] <= priority[Stack[-1]] :      # while priority of x is not greater than priority of element in the stack
                     Postfix.append( Stack.pop() )                                   # pop stack & add to Postfix
                 Stack.append(x)                                 # push x to stack
 
         print(x.center(8), (''.join(Stack)).ljust(print_width), (''.join(Postfix)).ljust(print_width), sep = " | ")         # Output in tabular format
 
-    while(len(Stack) > 0):                                  # while stack is not empty
+    while len(Stack) > 0 :                                  # while stack is not empty
         Postfix.append( Stack.pop() )                       # pop stack & add to Postfix
         print(' '.center(8), (''.join(Stack)).ljust(print_width), (''.join(Postfix)).ljust(print_width), sep = " | ")         # Output in tabular format
 
     return "".join(Postfix)             # return Postfix as str
 
-Infix = input("\nEnter an Infix Equation = ")       #Input an Infix equation
+Infix = input("\nEnter an Infix Equation (space separated) = ")       #Input an Infix equation
 Infix = "".join(Infix.split())                      #Remove spaces from the input
 print("\n\t", Infix, "->", infix_2_postfix(Infix))
