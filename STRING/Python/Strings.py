a88497e9a29e9d6dd2b828ac3f39471517eb5480
ashwek <ashwekswamy@protonmail.com>
2019-01-03 16:16:38 +0530

    Create Strings.py

---
+++ STRING/Python/Strings.py
@@ -0,0 +1,33 @@
+trial = "HELLO WORLD This is github"
+
+print("\nstring : ", trial)
+
+print("\nMethods in \'str\' class :")
+
+print("capitalize() :".rjust(27), trial.capitalize())
+
+print("center(50, '*') :".rjust(27), trial.center(50, '*'))
+
+print("count('i') :".rjust(27), trial.count('i'))
+
+print("endswith('hub') :".rjust(27), trial.endswith('hub'))
+
+print("find('i') :".rjust(27), trial.find('i'))
+
+print("islower() :".rjust(27), trial.islower())
+
+print("ljust(50, '-') :".rjust(27), trial.ljust(50, '-'))
+
+print("lower() :".rjust(27), trial.lower())
+
+print("replace(' ', '_') :".rjust(27), trial.replace(' ', '_'))
+
+print("rjust(50, '+') :".rjust(27), trial.rjust(50, '+'))
+
+print("split() :".rjust(27), trial.split())
+
+print("swapcase() :".rjust(27), trial.swapcase())
+
+print("title() :".rjust(27), trial.title())
+
+print("upper() :".rjust(27), trial.upper())
