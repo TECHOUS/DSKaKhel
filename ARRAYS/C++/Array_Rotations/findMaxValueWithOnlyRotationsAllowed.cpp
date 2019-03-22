a1fc3edef2f267942a2df125f2fda183e2ec08d0
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2019-03-22 09:29:58 +0530

    added max sum with only rotations allowed proj

--- ARRAYS/C++/Array_Rotations/findMaxValueWithOnlyRotationsAllowed.cpp
+++ ARRAYS/C++/Array_Rotations/findMaxValueWithOnlyRotationsAllowed.cpp
@@ -4,10 +4,95 @@
 
 using namespace std;
 
+/* 
+                    A = 1 20 2 10
 
-void findMaxWithOnly_Rotations(int *arr,int size){
+                    after 2 rotations 
+
+                    2 10 1 20
+
+                    the sum becomes 72
+
+                    which is max;
+
+
+                    COMPLEXITIES:
+
+                    TimeComplexity is most probably 2O(n*k), where k = no of rotations
+                    SpaceComplexity is O(m+n);
+
+
+
+*/
+
+/* Brute force method */
+void printSumArr(int * , int);
+int Calculate_Rotation_Sum(int * arr, int size){
+    int eachRotationSum = 0; 
+    for(int i = 0 ; i < size ; i++ ){
+        eachRotationSum += arr[i]*i;
+    }
+
+    return eachRotationSum;
+}
+
+void RotateArray(int * arr ,int size,int k){
+    while(k--){
+        int temp = arr[0];
+
+        for(int i = 0 ; i < size - 1 ; i++){
+            arr[i] = arr[i+1];
+        }
+
+        arr[size-1] = temp;
+    } // O(n*k);
+
+}
+
+int findMaxWithOnly_Rotations(int *arr,int size){
     
+    int firstSum = 0,j=0,rotations = 1,foundMax = 0;
+    int *rotationSum = new int[size];
+    /* find all rotations and sum them */
+    
+    for(int i = 0 ; i < size ; i++){
+            firstSum += arr[i]*i; 
+    } //O(n)
+    rotationSum[j] = firstSum;
+    j = 1;
+
+    while(rotations < size){
+    RotateArray(arr,size,rotations);
+    rotationSum[j++] = Calculate_Rotation_Sum(arr,size);
+    rotations++;
+    }
+
+   // printSumArr(rotationSum , size);
+    for(int i = 0 ; i < j ; i++){
+            foundMax = max(rotationSum[i],foundMax);
+        }
+
+    return foundMax;
+}
+
+void printSumArr(int * arr , int size){
+
+    for(int i = 0 ; i < size ; i++){
+        cout<<"The sum at index "<<i<<" is "<<arr[i]<<endl;
+    }
 }
 int main(){
 
+    int size,*arr;
+    cout<<"Enter the size"<<endl;
+    cin>>size;
+    arr = new int[size];
+
+    cout<<"Enter the array"<<endl;
+    for(int i = 0 ; i < size ; i++){
+        cin>>arr[i];
+    }
+
+    int Max_Sum = findMaxWithOnly_Rotations(arr,size);
+    cout<<"The max sum is "<<Max_Sum<<endl;
 }
